93c581fc9ea7a42de1919d92d2e38155299458f2
mrandri19 <andrecogno@hotmail.it>
2015-10-26 18:39:50 +0100

    Add list-fonts option

--- lib.py
+++ lib.py
@@ -40,36 +40,63 @@ def currentColorscheme():
     f = open(os.path.join(FILE_PATH,FILE_NAME),'r')
     content = f.read()
 
-    current = content.splitlines()[3]
+    lines = content.splitlines()
+    for line in lines:
+        if ".Xcolors" in line:
+            #The starting string:
+            #include "/home/andrea/.config/X11/.Xcolors.XXXX"
+            #return just XXXX
+            line = line.replace("#include",'')\
+                    .replace(FILE_PATH,'')\
+                    .replace('.Xcolors', '')\
+                    .strip(" ")\
+                    [1:-1]\
+                    .strip('.')
+            return line
+    return False
 
-    #The starting string:
-    #include "/home/andrea/.config/X11/.Xcolors.XXXX"
-    #return just XXXX
-    current = current.replace("#include",'')\
-            .replace(FILE_PATH,'')\
-            .replace('.Xcolors', '')\
-            .strip(" ")\
-            [1:-1]\
-            .strip('.')
-    return current
+def findFonts():
+    fonts = []
+
+    f = open(os.path.join(FILE_PATH,FILE_NAME),'r')
+    content = f.read()
+
+    lines = content.splitlines()
+    for line in lines:
+        if "URxvt.font" in line:
+            #The starting string
+            #URxvt.font : xft:Ubuntu Mono derivative Powerline:size=12
+            #The final Ubuntu Mono derivative Powerline
+            line = line.replace("URxvt.font :", "")
+            line = line.replace("!", "")
+            line = line.replace("xft:", "")
+            line = line.split(":")[0]
+            line = line.strip()
+            line += "\n"
+            fonts.append(line)
+    return fonts
 
 @click.command()
 @click.option('-c', '--colorscheme', help='Set the colorscheme')
-@click.option('-l', '--list', help='List all the available colorschemes',\
+@click.option('-lc', '--list-colorschemes', help='List all the available colorschemes',\
         is_flag=True)
-@click.option('--current', help='Display the current colorscheme', is_flag=True)
-def cli(colorscheme, list, current):
+@click.option('-cc', '--current-colorscheme', help='Display the current colorscheme', is_flag=True)
+@click.option('-lf', '--list-fonts', help='', is_flag=True)
+def cli(colorscheme, list_colorschemes, current_colorscheme, list_fonts):
 
     if colorscheme is not None :
         updateColorscheme(currentColorscheme(), colorscheme)
         #Update Xresources
         run("xrdb -load /home/andrea/.config/X11/.Xresources", shell=True, check=True)
 
-    if list is not False:
-        click.echo("".join(findColorschemes()).rstrip('\n'))
+    if list_colorschemes is True:
+        click.echo("".join(findColorschemes()).rstrip("\n"))
 
-    if current is not False:
+    if current_colorscheme is True:
         click.echo(currentColorscheme())
+
+    if list_fonts is True:
+        click.echo("".join(findFonts()).rstrip("\n"))
 
 
 
