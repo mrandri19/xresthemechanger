56075325d4e439e9adbd8f84a8794d54eda7ad02
mrandri19 <andrecogno@hotmail.it>
2015-12-11 22:42:54 +0100

    Made code pep8 compliant

--- lib.py
+++ lib.py
@@ -1,30 +1,33 @@
 #! /usr/bin/python3.5
-import os, click
+import os
+import click
 from subprocess import run
 
 FILE_NAME = '.Xresources'
 FILE_PATH = "/home/andrea/.config/X11/"
 
+
 def getFile(mode):
-    #Read from Xresources
+    # Read from Xresources
     f = open(os.path.join(FILE_PATH, FILE_NAME), mode)
     return f.read()
+
 
 def updateColorscheme(old_colorscheme, new_colorscheme):
 
     f = open(os.path.join(FILE_PATH, FILE_NAME), 'r+')
     content = getFile('r+')
 
-    #Replace the colorscheme and write
-    new_content = content.replace(old_colorscheme ,new_colorscheme)
+    # Replace the colorscheme and write
+    new_content = content.replace(old_colorscheme, new_colorscheme)
 
-    #Clear the file before writing
+    # Clear the file before writing
     f.seek(0)
     f.truncate()
-    f.write(new_content);
+    f.write(new_content)
 
-    #Write the backup
-    old_file = open(os.path.join(FILE_PATH,'.Xresources.bak'), 'w+')
+    # Write the backup
+    old_file = open(os.path.join(FILE_PATH, '.Xresources.bak'), 'w+')
     old_file.write(content)
 
 
@@ -32,7 +35,7 @@ def findColorschemes():
     colorschemes = []
     content = os.listdir(FILE_PATH)
 
-    #Search in the directory and create a list with the colorschemes
+    # Search in the directory and create a list with the colorschemes
     for filename in content:
         if '.Xcolors' in filename:
             filename = filename.replace('.Xcolors.', '')
@@ -40,24 +43,25 @@ def findColorschemes():
             colorschemes.append(filename + '\n')
     return colorschemes
 
+
 def currentColorscheme():
-    #Read from Xresources and return the third line
+    # Read from Xresources and return the third line
     content = getFile('r')
 
     lines = content.splitlines()
     for line in lines:
         if ".Xcolors" in line:
-            #The starting string:
-            #include "/home/andrea/.config/X11/.Xcolors.XXXX"
-            #return just XXXX
-            line = line.replace("#include",'')\
-                    .replace(FILE_PATH,'')\
-                    .replace('.Xcolors', '')\
-                    .strip(" ")\
-                    [1:-1]\
-                    .strip('.')
+            # The starting string:
+            # include "/home/andrea/.config/X11/.Xcolors.XXXX"
+            # return just XXXX
+            line = line.replace("#include", '')\
+                .replace(FILE_PATH, '')\
+                .replace('.Xcolors', '')\
+                .strip(" ")[1:-1]\
+                .strip('.')
             return line
     return False
+
 
 def currentFont():
     fonts = []
@@ -67,9 +71,9 @@ def currentFont():
     lines = content.splitlines()
     for line in lines:
         if "URxvt.font" in line:
-            #The starting string
-            #URxvt.font : xft:Ubuntu Mono derivative Powerline:size=12
-            #The final Ubuntu Mono derivative Powerline
+            # The starting string
+            # URxvt.font : xft:Ubuntu Mono derivative Powerline:size=12
+            # The final Ubuntu Mono derivative Powerline
             line = line.replace("URxvt.font :", "")
             line = line.replace("!", "")
             line = line.replace("xft:", "")
@@ -79,19 +83,27 @@ def currentFont():
             fonts.append(line)
     return fonts
 
+
 @click.command()
 @click.option('-c', '--colorscheme', help='Set the colorscheme')
-@click.option('-lc', '--list-colorschemes', help='List all the available colorschemes',\
-        is_flag=True)
-@click.option('-cc', '--current-colorscheme', help='Display the current colorscheme', is_flag=True)
-@click.option('-cf', '--current-font', help='Display the current font', is_flag=True)
-@click.option('-e', '--edit-xresources', help='Opens $EDITOR to edit the .Xresources file', is_flag=True)
-def cli(colorscheme, list_colorschemes, current_colorscheme, current_font, edit_xresources):
+@click.option('-lc', '--list-colorschemes',
+              help='List all the available colorschemes',
+              is_flag=True)
+@click.option('-cc', '--current-colorscheme',
+              help='Display the current colorscheme',
+              is_flag=True)
+@click.option('-cf', '--current-font', help='Display the current font',
+              is_flag=True)
+@click.option('-e', '--edit-xresources',
+              help='Opens $EDITOR to edit the .Xresources file', is_flag=True)
+def cli(colorscheme, list_colorschemes, current_colorscheme, current_font,
+        edit_xresources):
 
-    if colorscheme is not None :
+    if colorscheme is not None:
         updateColorscheme(currentColorscheme(), colorscheme)
-        #Update Xresources
-        run("xrdb -load {}{}".format(FILE_PATH, FILE_NAME), shell=True, check=True)
+        # Update Xresources
+        run("xrdb -load {}{}".format(FILE_PATH, FILE_NAME), shell=True,
+            check=True)
 
     if list_colorschemes is True:
         click.echo("".join(findColorschemes()).rstrip("\n"))
@@ -104,6 +116,11 @@ def cli(colorscheme, list_colorschemes, current_colorscheme, current_font, edit_
 
     if edit_xresources is True:
         os.system('{} {}'.format(os.getenv('EDITOR'), (FILE_PATH+FILE_NAME)))
+
+    # No arguments
+    if colorscheme is None and not list_colorschemes and not current_colorscheme\
+            and not current_font and not edit_xresources:
+                click.echo("Usage:")
 
 if __name__ == "__main__":
     cli()
